696b3408ea85b8c5d2c99b8f2291e4965a75d8b1
parkgrrr <parkgrrr@gmail.com>
2014-11-10 00:54:32 -0700

    Navigation Bug Fix
    The bug where the top left navigation button would crash on the search
    page has been fixed.

--- Android/Properties/AssemblyInfo.cs
+++ Android/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using Android.App;
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle ("StockApp.Android")]
+[assembly: AssemblyTitle ("Stock Quotr")]
 [assembly: AssemblyDescription ("")]
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
